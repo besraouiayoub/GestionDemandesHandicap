fc54fb237723fca87a8341b8712b50ccb4296b65
Ahmed61862 <rayhaniahmed013@gmail.com>
2026-05-15 15:41:37 +0100

    Add database connection

---
+++ src/uir/ac/gestionhandicape/database/DBConnection.java
@@ -0,0 +1,75 @@
+package uir.ac.gestionhandicape.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://127.0.0.1:3306/gestion_handicap?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private static DBConnection instance;
+
+    private Connection connection;
+
+    private DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            this.connection = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized DBConnection getInstance() {
+
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection() {
+
+        try {
+
+            if (connection == null || connection.isClosed()) {
+
+                connection = DriverManager.getConnection(
+                        URL,
+                        USER,
+                        PASSWORD
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public void closeConnection() {
+
+        try {
+
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
